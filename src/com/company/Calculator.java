f4d3d20d985354f168e587ebf08398d7debaa3ae
Denis <firsa.denis@gmail.com>
2020-03-25 15:58:59 +0300

    add div function

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -22,6 +22,8 @@ public class Calculator {
                 return degree(opperands[0], opperands[1]);
             case("log"):
                 return log(opperands[0], opperands[1]);
+            case ("div"):
+                return div(a,b);
             default:
                 throw new Exception("Unknown arithmetics sign");
         }
@@ -37,5 +39,19 @@ public class Calculator {
         BigDecimal b = BigDecimal.valueOf(Math.log(second));
 
         return a.divide(b).doubleValue();
+    }
+
+    private static double div(BigDecimal first, BigDecimal second){
+        double var = first.divide(second).doubleValue();
+        boolean lessThenZero = var < 0;
+        var = lessThenZero ? -1*var : var;
+        int div = 0;
+
+        while (var >= 1){
+            div++;
+            var--;
+        }
+
+        return lessThenZero ? -1*div : div;
     }
 }
